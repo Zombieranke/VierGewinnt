cdf3ba4b418745fd118d8351389dd0c611f9b6b3
Zombieranke <abcdefghijklmnop@qrs>
2015-04-02 19:22:04 +0200

    Alpha version of menu function

--- VierGewinnt/GameManager.cpp
+++ VierGewinnt/GameManager.cpp
@@ -7,6 +7,7 @@
  */
 
 #include <iostream>
+#include <string>
 #include "Board.h"
 #include "Player.h"
 
@@ -58,13 +59,85 @@ int main()
 
 int menu(int &numberOfRounds)
 {
-	int playerNumber;
+	int numberOfPlayers;
+	std::string tempPlayerName = "";
+	char tempColor = ' ';
 
-	std::cout << "           WELCOME TO CONNECTFOUR!\n";
-	std::cout << "            GAME OF THE YEAR 1922\n";
-	std::cout << "*****************************************************\n";
+	std::cout << "           WELCOME TO CONNECTFOUR!" << std::endl;
+	std::cout << "            GAME OF THE YEAR 1922" << std::endl;
+	std::cout << "*****************************************************" << std::endl;
 	std::cout << "How many human players do you wish to have?\nPlease enter the number here: ";
-	std::cin >> playerNumber;
+	std::cin >> numberOfPlayers;
+	std::cout << std::endl;
+	while(numberOfPlayers < 0 || numberOfPlayers > 2)
+	{
+		std::cout << "ConnectFour is a game for two players, please enter a value between 0 and 2 for the number of human players who participate: ";
+		std::cin >> numberOfPlayers;
+		std::cout << std::endl;
+	}
+
+	if(numberOfPlayers > 0)
+	{
+		std::cout << "Player 1, please enter your name: " << std::endl;
+		std::cin >> tempPlayerName;
+		std::cout << std::endl;
+
+		std::cout << "Which symbol would you like to use for your pieces?" << std::endl << "Enter one here: " << std::endl;
+		std::cin >> tempColor;
+		std::cout << std::endl;
+
+		while(tempColor == '.' || tempColor == ' ')
+		{
+			std::cout << "This symbol is not available. Please choose another one: " << std::endl;
+			std::cin >> tempColor;
+			std::cout << std::endl;
+		}
+
+		Human* p1 = Player(tempPlayerName,tempColor);
+
+			std::cout << p1->getName() <<" has chosen " << p1->getColor() << "." << std::endl;
+
+		if(numberOfPlayers == 2)
+		{
+			std::cout << "Player 2, please enter your name: " << std::endl;
+			std::cin >> tempPlayerName;
+			std::cout << std::endl;
+
+			std::cout << "Which symbol would you like to use for your pieces?" << std::endl << "Enter one here: " << std::endl;
+			std::cin >> tempColor;
+			std::cout << std::endl;
+
+
+			while(tempColor == p1->getColor() || tempColor == '.' || tempColor == ' ')
+			{
+				std::cout << "This symbol is not available. Please choose another one: " << std::endl;
+				std::cin >> tempColor;
+				std::cout << std::endl;
+			}
+
+			Human* p2 = Player(tempPlayerName, tempColor);
+
+			std::cout << p2->getName() <<" has chosen " << p2->getColor() << "." << std::endl;
+
+		}
+		else
+		{
+			ComputerPlayer* c1 = new ComputerPlayer();
+
+			std::cout << c1->getName() <<" has chosen " << c1->getColor() << "." << std::endl;
+
+		}
+	}
+	else
+	{
+		ComputerPlayer* c1 = new ComputerPlayer();
+
+		std::cout << c1->getName() <<" has chosen " << c1->getColor() << "." << std::endl;
+
+		ComputerPlayer* c2 = new ComputerPlayer();
+
+		std::cout << c2->getName() <<" has chosen " << c2->getColor() << "." << std::endl;
+	}
 
 
 }
